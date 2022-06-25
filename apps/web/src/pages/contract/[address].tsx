0f0c12265bf6b6a53e6c0328b213b1179497ed73
Róbert Istók <robertistok94@gmail.com>
2022-06-25 18:57:09 -0400

    Update [address].tsx

--- apps/web/src/pages/contract/[address].tsx
+++ apps/web/src/pages/contract/[address].tsx
@@ -1,8 +1,84 @@
+import { useRouter } from 'next/router';
 import { Container, Row, Col, Card, Text, Button } from '@nextui-org/react';
+import useAxios from 'axios-hooks';
+import { useProvider, useAccount } from 'wagmi';
+import { ethers } from 'ethers';
+
+import useReadContract from '../../hooks/useReadContract';
+import useContractWrite from '../../hooks/useWriteContract';
+import useAddress from '../../hooks/useAddress';
+import { useEffect, useState } from 'react';
+import { MyNFT__factory } from 'web3-config';
 
 const steps = [{ completed: true, id: 1, message: 'Jattebra NYC' }];
 
 const AddressExpanded = () => {
+  const router = useRouter();
+  const { address: contractAddress }: { address?: string } = router.query;
+  const provider = useProvider();
+  const { data: { address: connectedAddress } = {} } = useAccount();
+  const [contractBalance, setContractBalance] = useState<string>();
+  const [started, setStarted] = useState(false);
+  const [, abortProject] = useContractWrite(MyNFT__factory, 'abort');
+  const [, claimNfts] = useContractWrite(MyNFT__factory, 'claimAbort');
+
+  const [
+    { data: { assets: accountAssets = [] } = {}, loading },
+    getAccountAssets,
+  ] = useAxios(
+    {
+      url: '/api/opensea',
+      method: 'POST',
+      data: {
+        path: `assets?owner=${connectedAddress}&asset_contract_address=${contractAddress}`,
+      },
+    },
+    { manual: true }
+  );
+
+  const [
+    { data: collectionData, loading: collectionDataLoading },
+    getCollection,
+  ] = useAxios(
+    {
+      url: '/api/opensea',
+      method: 'POST',
+      data: {
+        path: `asset_contract/${contractAddress}`,
+      },
+    },
+    { manual: true }
+  );
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      const contractBalanceRes = await provider.getBalance(contractAddress);
+      setContractBalance(ethers.utils.formatEther(contractBalanceRes));
+    };
+
+    if (contractAddress) {
+      getCollection();
+      fetchBalance();
+      getAccountAssets();
+    }
+  }, [contractAddress]);
+
+  // const { data, error } = useReadContract(MyNFT__factory, 'b', {
+  //   params: [accountData?.address as string],
+  //   enabled: !!accountData?.address,
+  // });
+
+  useEffect(() => {
+    setStarted(true);
+  }, []);
+  if (!started) {
+    return null;
+  }
+
+  if (collectionDataLoading || !collectionData || loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Container
       fluid
@@ -22,12 +98,11 @@ const AddressExpanded = () => {
             css={{ flexFlow: 'nowrap' }}
           >
             <Col>
-              <Text h4>Contract nane</Text>
-              <Text h6>0x9AA48Bb538206d5D7329aafd17B63562e7c98457</Text>
+              <Text h4>{collectionData.name}</Text>
+              <Text h6>{contractAddress}</Text>
             </Col>
             <Col style={{ textAlign: 'right' }}>
-              <Text h4>Balance: 38ETH</Text>
-              <Text h6>Minted 50/100</Text>
+              <Text h4>Balance: {contractBalance} ETH</Text>
             </Col>
           </Container>
         </Card.Header>
@@ -40,8 +115,8 @@ const AddressExpanded = () => {
         </Card.Body>
         <Card.Footer>
           <Row justify="space-between">
-            <Button disabled>Abandon</Button>
-            <Button>Claim</Button>
+            <Button onClick={() => abortProject()}>Abandon</Button>
+            <Button onClick={() => claimNfts()}>Claim</Button>
           </Row>
         </Card.Footer>
       </Card>

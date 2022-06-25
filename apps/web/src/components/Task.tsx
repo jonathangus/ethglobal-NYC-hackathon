466c1bbf3fa15197b1851396c7c17c5e34bd0a8c
Keon <kwk236@gmail.com>
2022-06-25 19:00:43 -0400

    finish styling

--- apps/web/src/components/Task.tsx
+++ apps/web/src/components/Task.tsx
@@ -1,21 +1,32 @@
 import React from "react";
-import { Avatar, Grid } from "@nextui-org/react";
+import { Avatar, Grid, Text } from "@nextui-org/react";
 
 
 function Task({ step, title, description, isActive, isDone, isLast }) {
   return (
-    <Grid className="task">
-      <Avatar 
-        squared
-        className={isActive ? "step active" : "step"}
-        text={isDone ? "✔" : step.toString()}
-      >
-      </Avatar>
+    <Grid>
+      <Grid.Container className="task" direction="row">
+        <Avatar 
+          squared
+          className={isActive ? "step active" : "step"}
+          text={isDone ? "✔" : step.toString()}
+          color={isDone ? "success" : 'default'}
+        >
+        </Avatar>
 
-      <div className={isActive ? "task-info acitve-task-info" : "task-info"}>
-        <div className="title"> {title} </div>
-        <div className="description"> {description} </div>
-      </div>
+        <div style={{marginLeft: '12px'}}>
+          <Text
+            h5
+            // css={{
+            //   textGradient: "45deg, $blue600 -20%, $pink600 50%",
+            // }}
+            weight="bold"
+          >
+            {title} 
+          </Text>
+          <Text  size={12}> {description} </Text>
+        </div>
+      </Grid.Container>
     </Grid>
   );
 }

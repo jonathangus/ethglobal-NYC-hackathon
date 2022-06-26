9fdacb0c00cffc31a1ce7e53a2efc8a0499c9dff
Keon <kwk236@gmail.com>
2022-06-25 22:01:31 -0400

    update step ui

--- apps/web/src/components/Roadmap.tsx
+++ apps/web/src/components/Roadmap.tsx
@@ -2,29 +2,61 @@ import React, { useState } from "react";
 import Task from "../components/Task";
 import { Avatar, Grid } from "@nextui-org/react";
 
+
+// constructor()
+// ERC721('MyToken', 'MTK')
+// SafeLaunch(RESERVED_TREASURY_AMOUNT, MINT_PRICE)
+// {
+// addStep('governanceIsApproved()', 'Governance approval');
+// addStep('mintProgress()', 'Half minted');
+// addStep('apeIsSent()', 'BAYC sent to user');
+// addStep('charityIsSent()', 'Charity sent');
+// }
+
+// function mintProgress() public view returns (bool) {
+// return MAX_SUPPLY / 2 < totalSupply();
+// }
+
+// function charityIsSent() public view returns (bool) {
+// return charitySent;
+// }
+
+// function governanceIsApproved() public view returns (bool) {
+// return governanceApproved;
+// }
+
+// function apeIsSent() public view returns (bool) {
+// return apeSent;
+// }
+
+
 function Roadmap({stepsCompleted}) {
   console.log("Steps Completed:", stepsCompleted)
 
   const [tasks] = useState([
     {
       id: 1,
-      title: "First",
-      description: "Lorem ipsum dolor sit amet.",
+      title: "Governance approval",
+      description: "governanceIsApproved()",
+      func: "governanceIsApproved"
     },
     {
       id: 2,
-      title: "Second",
-      description: "Lorem ipsum dolor sit",
+      title: "Half minted",
+      description: "mintProgress()",
+      func: "mintProgress"
     },
     {
       id: 3,
-      title: "Third",
-      description: "Lorem ipsum dolor sit sam",
+      title: "BAYC sent to user",
+      description: "apeIsSent()",
+      func: "apeIsSent"
     },
     {
       id: 4,
-      title: "Fourth",
-      description: "Lorem ipsum dolor sit amet.",
+      title: "Charity sent",
+      description: "charityIsSent()",
+      func: "charityIsSent"
     },
   ]);
 
@@ -36,6 +68,7 @@ function Roadmap({stepsCompleted}) {
             key={task.id}
             title={task.title}
             description={task.description}
+            func={task.func}
             step={index + 1}
             isDone={stepsCompleted > index}
             isActive={stepsCompleted === index}
